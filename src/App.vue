446a27f1fd0b9ff56ebb8608ddf51cd7c32e0abb
ViniciusDuarte17 <vinicius.benduarte14@gmail.com>
2023-02-11 11:11:53 -0300

    configuração inicial

---
+++ src/App.vue
@@ -0,0 +1,18 @@
+<template>
+  <main class="columns is-gapless is-multiline">
+   <h1>pokemon</h1>
+ 
+  </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from "vue";
+
+export default defineComponent( {
+  name: 'App',
+})
+</script>
+
+<style>
+
+</style>
