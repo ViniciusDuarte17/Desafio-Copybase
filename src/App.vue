0bdf7b46de3ea3560cd072acd12119cacf913698
ViniciusDuarte17 <vinicius.benduarte14@gmail.com>
2023-02-11 11:32:27 -0300

    feat: Header na barra lateral

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,21 @@
 <template>
   <main class="columns is-gapless is-multiline">
-   <h1>pokemon</h1>
- 
+    <div class="column is-one-quarter">
+      <SideBar />
+    </div>
+    <div class="column is-three-quarter">
+      ??
+    </div>
   </main>
 </template>
 
 <script lang="ts">
 import { defineComponent } from "vue";
+import SideBar from "./components/SideBar.vue";
 
-export default defineComponent( {
-  name: 'App',
+export default defineComponent({
+    name: "App",
+    components: { SideBar }
 })
 </script>
 
---
+++ src/components/SideBar.vue
@@ -0,0 +1,37 @@
+<template>
+    <main>
+        <header>
+            <img src="../assets/Logo.png" alt="Logo da pokedex">
+        </header>
+    </main>
+</template>
+
+<script lang="ts">
+import { defineComponent } from 'vue';
+
+
+export default defineComponent( {
+    name: 'SideBar'
+})
+
+</script>
+
+<style scoped>
+header {
+    padding: 1rem;
+    background:#ff9c18 ;
+    width: 100%;
+    height: 100vh;
+}
+
+img {
+    width: 70%;
+}
+
+@media only screen and (max-width: 768px) {
+    header {
+        padding: 2.5rem;
+        height: auto;
+    }
+}
+</style>
